6015685b2da037703c60b446e06159d85d1a6231
divyatapatil <103753000+divyatapatil@users.noreply.github.com>
2025-03-07 12:58:38 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,10 @@
+NSERT INTO Books (Title, Author, Genre, PublishedYear, Quantity)
+VALUES 
+('Harry Potter', 'J.K. Rowling', 'Fantasy', 1997, 5),
+('The Alchemist', 'Paulo Coelho', 'Fiction', 1988, 3),
+('Atomic Habits', 'James Clear', 'Self-Help', 2018, 4);
+
+INSERT INTO Members (Name, Email, Phone)
+VALUES 
+('Divyata Patil', 'divyata@example.com', '9876543210'),
+('Rahul Sharma', 'rahul@example.com', '8765432109');
